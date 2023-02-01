614fbfb09e8bee07c2a410875284f11b0b3f92c2
stephaneDecockLocastic <stephane@locastic.com>
2023-02-01 08:12:41 +0100

    Cleanup

--- src/TranslationMigration/Executor.php
+++ src/TranslationMigration/Executor.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Locastic\SymfonyTranslationBundle\TranslationMigration;
 
 use Doctrine\Persistence\ManagerRegistry;
-use Doctrine\Persistence\ObjectManager;
 use Exception;
 use Locastic\SymfonyTranslationBundle\Factory\TranslationMigrationFactoryInterface;
 use Locastic\SymfonyTranslationBundle\Model\Translation;
